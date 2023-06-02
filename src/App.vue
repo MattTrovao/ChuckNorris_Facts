ea43a0b93c82b5275f8f8dc6b1bda75b635c157e
MattTrovao <42043818+MattTrovao@users.noreply.github.com>
2023-06-02 15:11:06 -0300

    feat: inicio do desenvolvimento

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <SealOfTrue position="right" />
+  <SealOfTrue position="left" />
+
+  <div class="container">
+    <header class="header">
+      <h1 class="header__title">
+        Chuck Noris Super Real True Facts
+      </h1>
+    </header>
+
+
+    <Facts />
+  </div>
+</template>
+
+<script>
+import Facts from './components/Facts.vue'
+import SealOfTrue from './components/SealOfTrue.vue'
+
+
+export default {
+  components: {
+    SealOfTrue,
+    Facts,
+  },
+  setup(){}
+}
+
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/SealOfTrue.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="`warning ${position}`">
+    <img src="../assets/icon.svg" alt="WARNING" class="warning__icon">
+
+    <p class="warning__text">
+      Everything here is 100% true!!! <br/><br/>
+      If you don't believe, Chuck Noris will visit you!!!
+    </p>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  props: {
+    position: {
+      type: String,
+      required: true,
+    }
+  },
+  setup(props){
+    const position = ref(props.position)
+
+    return{
+      position,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
